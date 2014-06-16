2c8a2175b92accbac44f290d881f02cefaa0d7a3
vjunloc <vijaytyagi.vt@gmail.com>
2014-06-16 22:23:10 +0530

    Completed login, signup and logout

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ var request = require('request');
 var _ = require('lodash');
 var xml2js = require('xml2js');
 
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 
 var showSchema = new mongoose.Schema({
   _id: Number,
@@ -64,6 +68,38 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
 var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
 
+passport.serializeUser(function(user, done){
+  done(null, user.id );
+});
+
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user){
+    done(err, user);
+  });
+});
+
+passport.use( new LocalStrategy({ usernameField : 'email' }, function(email, password, done){
+  User.findOne({ email: email }, function(err, user){
+    if( err ) return done(err);
+
+    if( !user ) return done(null, false );
+
+    user.comparePassword(password, function(err, isMatch){
+      if( err ) return done(err);
+      if( isMatch ) return done(null, user);
+      return done(null, false);
+    });
+
+  });
+
+}));
+
+function ensureAuthenticated( req, res, next) {
+  if( req.isAuthenticated() ) next();
+  else 
+      res.send(401);
+}
+
 mongoose.connect('localhost');
 
 
@@ -74,8 +110,44 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+
+app.use(session({ secret: 'keyboard cat'}));
+app.use( passport.initialize() );
+app.use( passport.session() );
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(function(req, res, next){
+  if( req.user ){
+    res.cookie('user', JSON.stringify(req.user));
+  }
+  next();
+});
+
+app.post('/api/login', passport.authenticate('local'), function(req, res){
+  res.cookie('user', JSON.stringify(req.user));
+  res.send(req.user);
+});
+
+app.get('/api/logout', function(req, res) {
+  req.logout();
+  res.send(200);
+});
+
+app.post('/api/signup', function(req, res, next){
+
+  var user = new User({
+    email : req.body.email,
+    password : req.body.password
+  });
+
+  user.save(function(err){
+    if( err ) return next(err);
+    res.send(200);
+  });
+
+});
 
 app.get('/api/shows', function(req, res, next){
 	var query = Show.find();
