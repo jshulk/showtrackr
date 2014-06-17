7f1ccbb53da839b0562aa7b0da06b547e248111e
vjunloc <vijaytyagi.vt@gmail.com>
2014-06-17 05:55:32 +0000

    ready for deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var xml2js = require('xml2js');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var agenda = require('agenda')({ db : {address: 'localhost:27017/test' }});
+var agenda = require('agenda')({ db : {address: 'mongodb://vjunloc:shallowbay@ds031618.mongolab.com:31618/showtrackr' }});
 var sugar = require('sugar');
 var nodemailer = require('nodemailer');
 
@@ -104,7 +104,7 @@ function ensureAuthenticated( req, res, next) {
       res.send(401);
 }
 
-mongoose.connect('localhost');
+mongoose.connect('mongodb://vjunloc:shallowbay@ds031618.mongolab.com:31618/showtrackr');
 
 
 var app = express();
@@ -259,6 +259,10 @@ app.post('/api/shows', function(req, res, next) {
         }
         return next(err);
       }
+        
+       var alertDate = Date.create('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
+        
+        agenda.schedule(alertDate, ' send email alert ', show.name ).repeatEvery('1 week');
       res.send(200);
     });
   });
@@ -298,4 +302,45 @@ app.use(function(err, req, res, next){
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+agenda.define('send email alert', function(job, done){
+    Show.findOne({ name : job.attrs.data }).populate('subscribers').exec(function(err, show){
+       var emails = show.subscribers.map(function(user){
+          return user.email; 
+       }); 
+       var upcomingEpisode = show.episodes.filter(function(episode){
+          	return new Date(episode.firstAired) > new Date(); 
+       })[0];
+        
+        var smtpTransport = nodemailer.createTransport('SMTP', {
+            service : 'SendGrid',
+            auth: { user: 'hslogin', pass: 'hspassword00'}
+        });
+        
+        var mailOptions = {
+            from : 'Fred foo <foo@blurdybloop.com',
+            to : emails.join(','),
+            subject: show.name + ' is starting soon',
+            text: show.name + ' starts in less than 2 hours on '+ show.network+'.\n\n'+
+            	'Episode '+ upcomingEpisode.episodeNumber + ' Overview \n\n '+ upcomingEpisode.overview
+        };
+        
+        smtpTransport.sendMail( mailOptions, function(error, response){
+           console.log('Message sent '+ response.message);
+            smtpTransport.close();
+            done();
+        });
+        
+    }); 
+});
+
+agenda.start();
+
+agenda.on('start', function(job){
+   	console.log("Job %s starting ", job.attrs.name ); 
+});
+
+agenda.on('complete', function(job){
+   	console.log("Job %s finished ", job.attrs.name ); 
 });
